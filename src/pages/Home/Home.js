8424a4d0f90dca6513322e21a35fd6e65ca79238
aishayanezdelaguardia@alumno.ieselrincon.es <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-04-25 17:13:34 +0100

    End class

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,21 +2,29 @@ import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import './Home.css'
 
+const imgList = [
+	{
+		id:1,
+		imgUrl:'./img/flights.jpg'
+	}
+]
+
+// style={{background: ´url(./img/flights.jpg)´}}
+
 function Home() {
-    return (
-        <>
-            <Header />
-            <div class="main-container">
-                <div class="img-container">
-                    <div class="img-flights">
-                        <button class="btn" type="button">Vuelos</button>
-                        <p>Home</p>
-                    </div>
-                </div>
-            </div>
-            <Footer />
-        </>
-    );
+	return (
+		<>
+			<Header />
+			<div class="main-container">
+				<div class="img-container-home">
+					<div class="img-flights" >
+						<button class="btn" type="button">Vuelos</button>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
 }
 
 export default Home;

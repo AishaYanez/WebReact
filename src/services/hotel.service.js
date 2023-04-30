24b1acc200669cebd11acb8e673128c96f2b37e4
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-04-30 00:08:21 +0000

    FIX RESPONSIVE

--- src/services/hotel.service.js
+++ src/services/hotel.service.js
@@ -3,8 +3,8 @@ const hotels = [
         id: 1,
         name: "Hotel Mareas",
         url: "mareas.jpg",
-        latitude: 27.9875508,
-        longitude: -15.3755913,
+        latitude: 28.0973453,
+        longitude: -15.4727408,
     }, {
         id: 2,
         name: "Villa Destino",
@@ -16,15 +16,15 @@ const hotels = [
         id: 3,
         name: "Villa Malui",
         url: "hawai.jpg",
-        latitude: 27.9875508,
-        longitude: -15.3755913,
+        latitude: 28.1056477,
+        longitude: -15.4523502,
     },
     {
         id: 4,
         name: "Resort Luxury",
         url: "resort.jpg",
-        latitude: 27.9875508,
-        longitude: -15.3755913,
+        latitude: 28.1286132,
+        longitude: -15.5093678,
     }
 ];
 

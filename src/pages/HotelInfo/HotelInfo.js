a34a450ea2e36e312efdc7780eea343af2928e4f
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-05-18 03:14:26 +0000

    MAP RENDERS BEFORE HOTEL INFORMATION

--- src/pages/HotelInfo/HotelInfo.js
+++ src/pages/HotelInfo/HotelInfo.js
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
 import HotelService from "../../services/hotel.service";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import ShowMap from "../../components/ShowMap/ShowMap";
 
 function HotelInfo() {
   const [hotel, setHotel] = useState({});
@@ -25,58 +26,21 @@ function HotelInfo() {
         console.error(err);
       });
   }
-
-  const removeHotel = () => {
-    HotelService.removeHotel(params.id);
-  }
-
-  const updateHotel = (updates) => {
-    HotelService.updateHotel(params.id, updates)
-      .then(() => {
-        console.log("Updated SUCCESSFULLY");
-      })
-      .catch((err) => {
-        console.log("Errot updating: ", err);
-      })
-  }
-
-  const handleSubmit = (evt) => {
-    //Actualiza la información de este hotel
-    evt.preventDefault();
-    const updates = {
-      name: evt.target.elements['name-hotel-form'].value,
-      url: evt.target.elements['img-hotel-form'].value,
-      latitude: evt.target.elements['latitude-hotel-form'].value,
-      longitude: evt.target.elements['longitude-hotel-form'].value
-    }
-    updateHotel(updates);
-  };
+  
+  useEffect(() => {
+    console.log("cargado")
+    getHotel();
+  }, []);
 
   const showHotelInfo = () => {
     return (
       <div >
         <h2>{hotel.name}</h2>
-        <a href="/explore" className='btn-delete' onClick={removeHotel}>Borrar</a>
-        <form className='form-FormComponent' onSubmit={handleSubmit}>
-          <label htmlFor='name-hotel-form'>Nombre:</label>
-          <input defaultValue={hotel.name} type='text' id='name-hotel-form' name='name-hotel-form' required />
-          <label htmlFor='img-hotel-form'>Imágen:</label>
-          <input defaultValue={hotel.url} type='text' id='img-hotel-form' name='img-hotel-form' required />
-          <label htmlFor='latidude-hotel-form'>Latitude:</label>
-          <input defaultValue={hotel.latitude} type='text' name="latitude-hotel-form" required />
-          <label htmlFor='-hotel-form'>Longitude:</label>
-          <input defaultValue={hotel.longitude} type='text' name="longitude-hotel-form" required />
-          <div className='btn-container'>
-          <button className='btn-update-form btn-form' type='submit'>Actualizar</button>
-          </div>
-        </form>
+								<ShowMap hotel={hotel} />
+        <p>{hotel.latitude}</p>
       </div>
     );
   }
-
-  useEffect(() => {
-    getHotel();
-  }, []);
 
   return (
     <>

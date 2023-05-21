b037fbeb1829cdfa2d16955ae7b52d2d139cf895
AishaYanez <aishayanezdelaguardia@alumno.ieselrincon.es>
2023-05-21 13:28:41 +0000

    RSS

--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -9,9 +9,21 @@ function News() {
       <div className="news-container">
       <h1>Noticias varias</h1>
         <div>
-          <h2></h2>
-          <p></p>
-          <p><a></a></p>
+          <h2>Descuento de fin de temporada</h2>
+          <p>Aprovecha la bajada de septiembre</p>
+          <p><a href='https://webreact-920d3.web.app//assets/RSS/RSS.xml'>Descuento</a></p>
+          <div><img src={img}/></div>
+        </div>
+        <div>
+          <h2>Descuento especial</h2>
+          <p>Para aquellos clientes cuyo número de asociado acabe en XXX, tenemos una oferta especial</p>
+          <p><a href='https://webreact-920d3.web.app//assets/RSS/RSS.xml'>Descuento</a></p>
+          <div><img src={img}/></div>
+        </div>
+        <div>
+          <h2>Nuevos destinos</h2>
+          <p>Aumento de los lugares que tenemos a tu disposición</p>
+          <p><a href='https://webreact-920d3.web.app//assets/RSS/RSS.xml'>Novedad</a></p>
           <div><img src={img}/></div>
         </div>
     </div>
